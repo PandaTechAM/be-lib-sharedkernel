6e1e42f21742641536eb2fa5773971f0a7886893
Haik <haik.asatryan.95@gmail.com>
2024-11-22 18:39:16 +0400

    health check changes

---
+++ src/SharedKernel/Constants/EndpointConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace SharedKernel.Constants;
+
+internal static class EndpointConstants
+{
+   internal const string TagName = "above-board";
+   internal const string BasePath = $"/{TagName}";
+}
