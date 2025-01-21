183d72f01b54f6460d8d6544455ddc32166a6874
HaikAsatryan <haik.asatryan.95@gmail.com>
2025-01-21 21:49:12 +0400

    dictionary extensions added

---
+++ src/SharedKernel/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace SharedKernel.Extensions;
+
+public static class DictionaryExtensions
+{
+   public static TValue? GetOrAdd<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key, TValue? value)
+      where TKey : notnull
+   {
+      ref var val = ref CollectionsMarshal.GetValueRefOrAddDefault(dict, key, out var exists);
+
+      if (exists)
+      {
+         return val;
+      }
+
+      val = value;
+      return val;
+   }
+
+   public static bool TryUpdate<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key, TValue value)
+      where TKey : notnull
+   {
+      ref var val = ref CollectionsMarshal.GetValueRefOrNullRef(dict, key);
+      if (Unsafe.IsNullRef(ref val))
+      {
+         return false;
+      }
+
+      val = value;
+      return true;
+   }
+}
