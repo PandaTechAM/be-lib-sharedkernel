69ecb71cdd98b8de69a06e76efe20e4964f789a2
Yelena Shahparonyan <yelena.shahparonyan@pandatech.it>
2025-10-09 17:17:53 +0400

    adding commission validation

---
+++ src/SharedKernel/ValidatorAndMediatR/Validators/CommissionRuleValidator.cs
@@ -0,0 +1,52 @@
+﻿using CommissionCalculator.DTO;
+using FluentValidation;
+using FluentValidation.Validators;
+using SharedKernel.ValidatorAndMediatR.Validators.Files;
+
+namespace SharedKernel.ValidatorAndMediatR.Validators;
+public sealed class CommissionRuleValidator<T> : PropertyValidator<T, CommissionRule?>
+{
+   public override string Name => "CommissionRuleValidator";
+
+   public override bool IsValid(ValidationContext<T> context, CommissionRule? rule)
+   {
+      if (rule == null || rule.CommissionRangeConfigs.Count == 0)
+      {
+         context.AddFailure("The ranges list cannot be null or empty.");
+         return false;
+      }
+
+      if (rule.CommissionRangeConfigs.Any(
+             r => r is { Type: CommissionType.Percentage, CommissionAmount: < -10 or > 10 }))
+      {
+         context.AddFailure(
+            "For 'Percentage' CommissionType, the CommissionAmount should be between -10 and 10. Commissions over 1000% are not allowed.");
+         return false;
+      }
+
+      if (rule.CommissionRangeConfigs.Count == 1)
+      {
+         if (rule.CommissionRangeConfigs[0].RangeStart != 0 || rule.CommissionRangeConfigs[0].RangeEnd != 0)
+         {
+            context.AddFailure("In case of one range, both 'From' and 'To' should be 0.");
+            return false;
+         }
+
+         if (rule.CommissionRangeConfigs[0].MaxCommission != 0 && rule.CommissionRangeConfigs[0].MaxCommission <
+             rule.CommissionRangeConfigs[0].MinCommission)
+         {
+            context.AddFailure("MaxCommission should be greater than or equal to MinCommission.");
+            return false;
+         }
+
+         return true; //check
+      }
+      else
+      {
+         var rangeValidator = new CommissionRangeValidator<T>();
+         var rangeValidatorResult = rangeValidator.IsValid(context, rule);
+
+         return true && rangeValidatorResult;
+      }
+   }
+}
---
+++ src/SharedKernel/ValidatorAndMediatR/Validators/Files/CommissionRangeValidator.cs
@@ -0,0 +1,68 @@
+﻿using CommissionCalculator.DTO;
+using FluentValidation;
+using FluentValidation.Validators;
+
+namespace SharedKernel.ValidatorAndMediatR.Validators.Files;
+public sealed class CommissionRangeValidator<T> : PropertyValidator<T, CommissionRule?>
+{
+   public override string Name => "CommissionRangeValidator";
+
+   public override bool IsValid(ValidationContext<T> context, CommissionRule? rule)
+   {
+      var startRule = rule!.CommissionRangeConfigs.FirstOrDefault(r => r is { RangeStart: 0, RangeEnd: > 0 });
+      if (startRule == null)
+      {
+         context.AddFailure("There should be at least one rule where From = 0.");
+         return false;
+      }
+
+      if (startRule.MaxCommission != 0 && startRule.MaxCommission < startRule.MinCommission)
+      {
+         context.AddFailure("MaxCommission should be greater than or equal to MinCommission.");
+         return false;
+      }
+
+      var verifiedRules = 1;
+
+      var lastTo = startRule.RangeEnd;
+
+      while (true)
+      {
+         var nextRule = rule.CommissionRangeConfigs.FirstOrDefault(r => r.RangeStart == lastTo);
+         if (nextRule is null && lastTo != 0)
+         {
+            context.AddFailure($"Gap detected. No rule found for 'From = {lastTo}'.");
+            return false;
+         }
+
+         if (nextRule is not null && nextRule.RangeStart == nextRule.RangeEnd)
+         {
+            context.AddFailure("Invalid rule. 'From' and 'To' cannot be equal.");
+            return false;
+         }
+
+         if (nextRule is not null && nextRule.MaxCommission != 0 && nextRule.MaxCommission < nextRule.MinCommission)
+         {
+            context.AddFailure("MaxCommission should be greater than or equal to MinCommission.");
+            return false;
+         }
+
+         if (lastTo == 0)
+         {
+            break;
+         }
+
+         verifiedRules++;
+
+         lastTo = nextRule!.RangeEnd;
+      }
+
+      if (verifiedRules != rule.CommissionRangeConfigs.Count)
+      {
+         context.AddFailure("There is some nested or gap ranges in the rules.");
+         return false;
+      }
+
+      return true; //check
+   }
+}
