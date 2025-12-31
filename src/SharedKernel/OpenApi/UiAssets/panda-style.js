cc1063bdcc7d16be38b0d70a0c89c64d272108db
Haik <haik.asatryan.95@gmail.com>
2025-12-31 10:51:52 +0400

    swagger ui work in progress

--- src/SharedKernel/OpenApi/UiAssets/panda-style.js
+++ src/SharedKernel/OpenApi/UiAssets/panda-style.js
@@ -12,18 +12,4 @@ document.addEventListener('DOMContentLoaded', function () {
         newLink.href = faviconPath;
         document.head.appendChild(newLink);
     }
-
-    // Scroll modal to top when it appears
-    const observer = new MutationObserver(() => {
-        const modal = document.querySelector('.modal-ux-content');
-        if (modal) {
-            modal.scrollTop = 0;
-            observer.disconnect();
-        }
-    });
-
-    observer.observe(document.body, {
-        childList: true,
-        subtree: true,
-    });
 });
